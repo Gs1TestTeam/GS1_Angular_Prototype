6752dba7c1f607e1861db625e312e891ec4f409f
Gs1TestTeam <42390768+Gs1TestTeam@users.noreply.github.com>
2018-08-15 17:09:54 -0400

    navigation bar
    added service for navigation bar

---
+++ src/app/Menu-List.ts
@@ -0,0 +1,19 @@
+/********************************************************************************* 
+ * Project: GS1 Canada Angular Prototype
+ * File: Menu-List.ts
+ * Imitates a list of menu items for navigation bar
+ * 
+ * Created by Olga Arsenieva
+ * Date: 2018.08.15
+ * 
+********************************************************************************/  
+
+import { MenuItem } from './MenuItem';
+
+export const MenuList: MenuItem[] = [
+    { name: 'Analysis View1', link: 'list1'},
+    { name: 'Analysis View2', link: 'list2'},
+    { name: 'Analysis View3', link: 'list3'},
+    { name: 'cat scientist', link: 'link123'},
+    { name: 'About', link: 'aboutlink'}
+];
