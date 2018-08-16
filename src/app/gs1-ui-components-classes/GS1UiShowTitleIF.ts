3d03393a9a0a8f966f25fb5b6bcee12c5f2555c2
Avinash Singh <hiroc.singh@gmail.com>
2018-08-16 16:49:53 -0400

    Implemented Show Table..

--- src/app/gs1-ui-components-classes/GS1UiShowTitleIF.ts
+++ src/app/gs1-ui-components-classes/GS1UiShowTitleIF.ts
@@ -1,13 +1,16 @@
-/********************************************************************************* 
+/*********************************************************************************
  * Project: GS1 Canada Angular Prototype
  * File: GS1UiShowTitleIF.ts
- * 
- * 
+ *
+ *
  * Created by Olga Arsenieva
  * Created date: 2018.08.16
- * 
-********************************************************************************/  
+ *
+********************************************************************************/
 export interface GS1UiShowTitleIF extends GS1UIComponentsIF {
-
-
+  Col1Title: string;
+  Col2: string;
+  fieldData: Array<string>;
+  Col3: string;
+  labelData: Array<string>;
 }
